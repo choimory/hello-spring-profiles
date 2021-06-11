c55441e49950be239f01b0142cea89017cbbf79d
choimory <choimory@naver.com>
2021-06-12 00:24:58 +0900

    Initial commit

---
+++ src/main/java/com/choimory/hellospringprofiles/HelloSpringProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.hellospringprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringProfilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringProfilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choimory/hellospringprofiles/HelloSpringProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.hellospringprofiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringProfilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
